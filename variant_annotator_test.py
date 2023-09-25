59ce8d3a2929a954408374a6d5c1295cd59a8009
Paul T <4277512+pault-tech@users.noreply.github.com>
2023-09-25 21:12:35 +0000

    Revert "Use variant-tools"
    This reverts commit 7a7d0e35a7462c6465e03dedcd8451056716617f.

--- variant_annotator_test.py
+++ variant_annotator_test.py
@@ -5,9 +5,16 @@ from unittest import mock
 
 class TestVariantAnnotation(unittest.TestCase):
 
+    def test_get_variant_annotation(self):
+        self.assertEqual(variant_annotator.get_variant_annotation('homo_sapiens', 'rs699'), (230710048, 230710048, 'missense_variant', ['AGT']))
+        self.assertIsNone(variant_annotator.get_variant_annotation('homo_sapiens', 'rsid_missing'))
+
     def test_main(self):
-        with mock.patch('argparse.ArgumentParser') as mock_parser:
-            mock_parser.return_value.parse_args.return_value = mock.Mock(input_file='test_input.txt', output_file='test_output.txt', species='homo_sapiens', log='info')
+        with mock.patch('argparse.ArgumentParser') as mock_parser, \
+            mock.patch('requests.get') as mock_get:
+            mock_parser.return_value.parse_args.return_value = mock.Mock(input_file='test_input.txt', output_file='test_output.txt', species='homo_sapiens')
+            mock_get.return_value.ok = True
+            mock_get.return_value.content = b'[{"start": 123, "end": 456, "most_severe_consequence": "missense_variant", "transcript_consequences": [{"gene_symbol": "GENE1"}]}]'
             variant_annotator.main()
 
             # Check that the output file contains the expected annotations
